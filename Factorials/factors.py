7a6b2a84a1a3f69990c528c3a20d3e477797d6e7
Sherman-Firefly <efracw2007@gmail.com>
2024-10-16 17:31:16 +0800

    facots

---
+++ Factorials/factors.py
@@ -0,0 +1,11 @@
+import random
+import math
+
+val=random.randint(1,10)
+print("The random value is: ", val)
+
+factor=math.factorial(val)
+print("The factorial of", val, "is", factor)
+
+logs=math.log(val)
+print("The log of ", val, "is", logs)
